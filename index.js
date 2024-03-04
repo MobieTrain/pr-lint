3dc03cf65f867865cd83007afead0dd3cf75513e
sergio-domingues <sergio.apdom@gmail.com>
2024-03-04 16:58:47 +0000

    refactor: split code in functions

--- index.js
+++ index.js
@@ -1,37 +1,32 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
-try {
-  const titleRegexString = core.getInput('title-regex', { required: true });
-  const titleRegexFlagsString = core.getInput('title-regex-flags') || 'g';
-  const branchRegexString = core.getInput('branch-regex', { required: true });
-  const branchRegexFlagsString = core.getInput('branch-regex-flags') || 'g';
+const { validateTitleAndBranch } = require("./methods");
+
+function main() {
+  const titleRegexString = core.getInput("title-regex", { required: true });
+  const branchRegexString = core.getInput("branch-regex", { required: true });
 
   const title = String(github.context.payload.pull_request.title);
   const branch = String(github.context.payload.pull_request.head.ref);
-  const titleRegex = new RegExp(titleRegexString, titleRegexFlagsString)
-  const branchRegex = new RegExp(branchRegexString, branchRegexFlagsString)
+  const titleRegex = new RegExp(titleRegexString);
+  const branchRegex = new RegExp(branchRegexString);
 
   core.info(`Title: ${title}`);
   core.info(`Title Regex: ${titleRegex}`);
-
   core.info(`Branch: ${branch}`);
   core.info(`Branch Regex: ${branchRegex}`);
 
+  const error = validateTitleAndBranch({
+    branch,
+    branchRegex,
+    title,
+    titleRegex,
+  });
 
-  if (!branchRegex.test(branch)) {
-    core.setFailed('Branch doesn\'t match given regex.');
+  if (error) {
+    core.setFailed(error);
   }
-
-  if (!titleRegex.test(title)) {
-    core.setFailed('Title doesn\'t match given regex.');
-  }
-
-  if (!title.includes(branch)) {
-    core.setFailed('Title and branch are inconsistent');
-  }
-
-
-} catch (error) {
-  core.setFailed(error.message);
 }
+
+main();
