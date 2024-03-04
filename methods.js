7bc600fe77f00cce28c549a8c7debb781c83b712
sergio-domingues <sergio.apdom@gmail.com>
2024-03-04 16:59:23 +0000

    build: update dist

--- methods.js
+++ methods.js
@@ -8,11 +8,9 @@ function validateTitleAndBranch({ branch, branchRegex, title, titleRegex }) {
       return "Title doesn't match given regex.";
     }
 
-    // Extract the issue number from the title using regex
     const issueNumberMatches = title.match(/\[(\w+-\d+)\]/);
     const issueNumber = issueNumberMatches ? issueNumberMatches[1] : null;
 
-    // Check if the branch is a substring of the title and it matches the issue number format
     if (
       !issueNumber ||
       (branch !== issueNumber && !branch.startsWith(issueNumber + "-"))
